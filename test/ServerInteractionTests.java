582677b3f018f301563d3f8684fa5875f1c507b6
gonzalonunez <gonzi@tcpmiami.com>
2017-04-08 21:37:11 -0400

    Beginning server comm tests

---
+++ test/ServerInteractionTests.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by gonzalonunez on 4/8/17.
+ */
+public class ServerInteractionTests {
+
+
+    @Test
+    public void testMoveNumberValidity() {
+        
+    }
+
+
+}
